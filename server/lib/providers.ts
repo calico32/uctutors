78cf24af56f4c1ccb6a75491a52ca65578b04051
calico32 <devdoge1@gmail.com>
2023-11-25 12:22:41 -0500

    add inspect logger with debug information

--- server/lib/providers.ts
+++ server/lib/providers.ts
@@ -1,9 +1,29 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import pino from 'pino'
+import { inspect } from 'util'
 
 export const prisma = new PrismaClient().$extends(withAccelerate())
 
-export const logger = pino({
+export const logger: Logger = pino({
   level: process.env.LOG_LEVEL || 'debug',
-})
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      ignore: 'hostname,pid',
+    },
+  },
+}) as any
+
+logger.$inspect = (loc: any, obj?: any) => {
+  if (obj) {
+    logger.debug('%s: %s', loc, inspect(obj, false, 10, true))
+  } else {
+    logger.debug('%s', inspect(obj, false, 10, true))
+  }
+}
+
+export type Logger = pino.Logger & {
+  $inspect(loc: any, obj: any): void
+  $inspect(obj: any): void
+}
--- server/vite.config.ts
+++ server/vite.config.ts
@@ -1,4 +1,16 @@
+import { parse } from '@typescript-eslint/typescript-estree'
+import type ESTree from 'estree'
+import { walk } from 'estree-walker'
+import MagicString from 'magic-string'
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const __filename = fileURLToPath(new URL(import.meta.url))
+const __dirname = path.dirname(__filename)
+function relativePath(path: string) {
+  return path.replace(__dirname, '.')
+}
 
 export default defineConfig({
   // map @/* to ./lib/*
@@ -7,4 +19,47 @@ export default defineConfig({
       '@': '/lib',
     },
   },
+  plugins: [
+    {
+      name: 'logger-inspect-source-location',
+      enforce: 'pre',
+      transform(code, id) {
+        if (!id.startsWith(__dirname)) return
+
+        const ast = parse(code, {
+          loc: true,
+          range: true,
+        })
+        const magicString = new MagicString(code)
+
+        walk(ast as ESTree.Program, {
+          enter(node) {
+            if (node.type === 'CallExpression') {
+              if (node.callee.type === 'MemberExpression') {
+                if (
+                  node.callee.object.type === 'Identifier' &&
+                  node.callee.object.name === 'logger' &&
+                  node.callee.property.type === 'Identifier' &&
+                  node.callee.property.name === '$inspect'
+                ) {
+                  if (node.arguments.length === 1) {
+                    const { line, column } = node.loc!.start
+                    const debugString = `'${relativePath(id)}:${line}:${column + 1}'`
+                    const insertLocation = node.arguments[0].range![0]
+
+                    magicString.prependLeft(insertLocation, debugString + ', ')
+                  }
+                }
+              }
+            }
+          },
+        })
+
+        return {
+          code: magicString.toString(),
+          map: magicString.generateMap(),
+        }
+      },
+    },
+  ],
 })
